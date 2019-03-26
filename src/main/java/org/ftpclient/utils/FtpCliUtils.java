d73fab8733084006b312f5415778c245ee42b88f
fbiwenjingyu <fbiwenjingyu@163.com>
2019-03-26 10:50:39 +0800

    add some new method

--- src/main/java/org/ftpclient/utils/FtpCliUtils.java
+++ src/main/java/org/ftpclient/utils/FtpCliUtils.java
@@ -4,10 +4,15 @@ package org.ftpclient.utils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * ftp客户端工具类
@@ -93,8 +98,216 @@ public class FtpCliUtils {
 
         this.ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
         this.ftpClient.enterLocalPassiveMode();
+        initFtpBasePath();
     }
 
+    /**
+     * <p>Description:[连接ftp时保存刚登陆ftp时的路径]</p>
+     * @throws IOException
+     */
+    private void initFtpBasePath() throws IOException{
+        if(StringUtils.isEmpty(this.ftpBasePath)){
+            synchronized (this){
+                if(StringUtils.isEmpty(this.ftpBasePath)){
+                    this.ftpBasePath = ftpClient.printWorkingDirectory();
+                }
+            }
+        }
+    }
+
+    /**
+     * <p>Description:[ftp是否处于连接状态，是连接状态返回<tt>true</tt>]</p>
+     * @return 是连接状态返回<tt>true</tt>
+     */
+    public boolean isConnected(){
+        return ftpClient.isConnected();
+    }
+
+    /**
+     * <p>Description:[上传文件到对应日期文件下，
+     * 如当前时间是2018-06-06，则上传到[ftpBasePath]/[DAILY_FILE_PATH]/2018/06/06/下]</p>
+     * Created on 2018/6/6
+     *
+     * @param fileName    文件名
+     * @param inputStream 文件输入流
+     * @return java.lang.String
+     */
+    public String upFileToDailyDir(String fileName, InputStream inputStream) throws IOException{
+        changeWorkingDirectory(ftpBasePath);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("/yyyy/MM/dd");
+        String formatDatePath = dateFormat.format(new Date());
+        String uploadDir = DAILY_FILE_PATH + formatDatePath;
+        makeDirs(uploadDir);
+        storeFile(fileName, inputStream);
+        return formatDatePath + "/" + fileName;
+
+    }
+
+    private void changeWorkingDirectory(String ftpBasePath) {
+        try {
+            this.ftpClient.changeWorkingDirectory(ftpBasePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void storeFile(String fileName, InputStream inputStream) {
+        try {
+            this.ftpClient.appendFile(fileName,inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * <p>Description:[下载ftp服务器下文件夹到本地]</p>
+     * @param remotePath ftp上文件夹路径名称
+     * @param localPath  本地上传的文件夹路径名称
+     * @return void
+     */
+    public void downLoadDir(String remotePath,String localPath) throws IOException{
+        localPath = localPath.replaceAll("\\\\","/");
+        File file = new File(localPath);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        FTPFile[] ftpFiles = this.ftpClient.listFiles(remotePath);
+        for (FTPFile ftpFile:ftpFiles) {
+            if(ftpFile != null){
+                if(ftpFile.isDirectory() && !ftpFile.getName().equals(".") && !ftpFile.getName().equals("..")){
+                    downLoadDir(remotePath + "/" + ftpFile.getName(),localPath + "/" + ftpFile.getName());
+                }else if(ftpFile.isFile()){
+                    downLoad(remotePath + "/" + ftpFile.getName(),new File(localPath + "/" + ftpFile.getName()));
+                }
+            }
+        }
+
+    }
+
+    private void downLoad(String remotePath, File localPath) {
+        try {
+            this.ftpClient.appendFile(remotePath, new FileInputStream(localPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * <p>Description:[列出ftp上文件目录下的文件]</p>
+     * @param filePath ftp上文件目录
+     * @return java.util.List<java.lang.String>
+     */
+    public List<String> listFileNames(String filePath) throws IOException{
+        FTPFile[] ftpFiles = this.ftpClient.listFiles(filePath);
+        List<String> fileList = new ArrayList<>();
+        if(ftpFiles != null){
+            for (FTPFile ftpFile: ftpFiles) {
+                if(ftpFile.isFile()){
+                    fileList.add(ftpFile.getName());
+                }
+            }
+        }
+        return fileList;
+    }
+
+    /**
+     * <p>Description:[发送ftp命令到ftp服务器中]</p>
+     * @param args ftp命令
+     */
+    public void sendSiteCommand(String args) throws IOException{
+        if(!this.ftpClient.isConnected()){
+            this.ftpClient.sendSiteCommand(args);
+        }
+    }
+
+    /**
+     * <p>Description:[获取当前所处的工作目录]</p>
+     * @return java.lang.String 当前所处的工作目录
+     */
+    public String printWorkingDirectory(){
+        if(!this.ftpClient.isConnected()){
+            return "";
+        }
+        try {
+            return this.ftpClient.printWorkingDirectory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * <p>Description:[切换到当前工作目录的父目录下]</p>
+     *
+     * @return boolean 切换成功返回true
+     */
+    public boolean changeToParentDirectory(){
+        if(!this.ftpClient.isConnected()){
+            return false;
+        }
+        try {
+            return this.ftpClient.changeToParentDirectory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * <p>Description:[返回当前工作目录的上一级目录]</p>
+     * @return java.lang.String 当前工作目录的父目录
+     */
+    public String printParentDirectory(){
+        if(!this.ftpClient.isConnected()){
+            return "";
+        }
+        String w = printWorkingDirectory();
+        changeToParentDirectory();
+        String p = printWorkingDirectory();
+        changeWorkingDirectory(w);
+        return p;
+    }
+
+    /**
+     *  <p>Description:[创建目录]</p>
+     * @param pathname 路径名
+     * @return 创建成功返回true
+     * @throws IOException
+     */
+    public boolean makeDirectory(String pathname) throws  IOException{
+        return this.ftpClient.makeDirectory(pathname);
+    }
+
+    /**
+     * <p>Description:[创建多个目录]</p>
+     *
+     * @param pathName 路径名
+     */
+    public void makeDirs(String pathName) throws IOException{
+        pathName = pathName.replaceAll("\\\\","/");
+        String[] pathNameArray = pathName.split("/");
+        for (String each : pathNameArray) {
+            if(StringUtils.isNotEmpty(each)){
+                this.ftpClient.makeDirectory(each);
+                this.ftpClient.changeWorkingDirectory(each);
+            }
+        }
+    }
+
+
+    /**
+     * <p>Description:[关闭流]</p>
+     * @param stream 流
+     */
+    private static void closeStream(Closeable stream){
+        if(stream != null){
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 
     /**
