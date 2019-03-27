74ef1bf75c88c7b9c4c83c9b5adcf920bd7877ba
fbiwenjingyu <fbiwenjingyu@163.com>
2019-03-27 19:59:56 +0800

    update

--- src/main/java/org/ftpclient/App.java
+++ src/main/java/org/ftpclient/App.java
@@ -1,13 +1,44 @@
 package org.ftpclient;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
         System.out.println( "Hello World!" );
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Class<?> clazz = classLoader.loadClass("java.lang.String");
+        Constructor<?> constructor = clazz.getConstructor(new Class[]{String.class});
+        Object instance = null;
+        try {
+            instance = constructor.newInstance("hello world");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        System.out.println(instance);
+
+        methodB();
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void methodA() {
+        methodB();
+    }
+
+    @Transactional(propagation = Propagation.MANDATORY)
+    public static void methodB() {
+        // do something
     }
 }
